26854bf98e3c2fa786443a955359fcb955b29a0d
Shashi Shekhar <shashi20008@gmail.com>
2019-01-20 13:08:34 +0530

    add the apis to fetch model

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const NavigateFS = require('./nav-fs');
-const http = require('http');
+const app = require('express')();
 
-const root = '/media/collection/'
+const root = '/media/collection/';
 
-//NavigateFS(root);
+NavigateFS(root);
 
-const server = http.createServer(function(req, res) {
-  console.log('hello world');
-  res.end('hello world');
+app.use(function(req, res, next) {
+  console.log(req.path);
+  //res.end('hello world');
+  next();
 });
 
-server.listen(8080);
+app.get('/model', function(req, res) {
+  res.json(NavigateFS.getModel());
+});
+
+app.get('/series', function(req, res) {
+  res.json(NavigateFS.getModel().series);
+});
+
+app.get('/series/list', function(req, res) {
+  res.json(Object.keys(NavigateFS.getModel().series));
+});
+
+app.get('/series/:id', function(req, res) {
+  res.json(NavigateFS.getModel().series[req.params.id]);
+});
+
+app.get('/movies', function(req, res) {
+  res.json(NavigateFS.getModel().movies);
+});
+
+app.get('/movies/list', function(req, res) {
+  res.json(Object.keys(NavigateFS.getModel().movies));
+});
+
+app.get('/movies/:id', function(req, res) {
+  res.json(NavigateFS.getModel().movies[req.params.id]);
+});
+
+app.listen(8080);
